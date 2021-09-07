b884f4fb7e588f1f50f2460926d346166e0cc260
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2021-09-07 23:24:35 +0530

    Customisez TabView Ready

--- lib/screens/tabSection.dart
+++ lib/screens/tabSection.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../colors.dart';
 
 Widget tabSection(BuildContext context) {
   return DefaultTabController(
@@ -7,40 +10,126 @@ Widget tabSection(BuildContext context) {
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
         Container(
-          child: TabBar(tabs: [
-            Tab(text: "प्रार्थना"),
-            Tab(text: "भजन"),
-            Tab(text: "रूपावली"),
-            Tab(text: "गजर"),
-            Tab(text: "अभंग"),
-            Tab(text: "गवळण"),
-            Tab(text: "extra"),
-          ]),color: Colors.black,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            child: DefaultTextStyle(
+              style: TextStyle(color: Colors.black),
+              child: TabBar(
+                  isScrollable: true,
+                  indicatorColor: Colors.yellow,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicatorWeight: 5,
+                  labelStyle: TextStyle(fontWeight: FontWeight.w800),
+                  tabs: [
+                    Tab(
+                      child: Text(
+                        "प्रार्थना",
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                    Tab(
+                      child: Text(
+                        "भजन",
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                    Tab(
+                      child: Text(
+                        "रूपावली",
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                    Tab(
+                      child: Text(
+                        "गजर",
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                    Tab(
+                      child: Text(
+                        "अभंग",
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                    Tab(
+                      child: Text(
+                        "गवळण",
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                    Tab(
+                      child: Text(
+                        "extra",
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                  ]),
+            ),
+          ),
         ),
         Container(
           //Add this to give height
           child: Expanded(
             child: TabBarView(children: [
               Container(
-                child: Text("Home Body"),
+                child: Column(
+                  children: [
+                    Divider(color: Colors.black),
+                    Text("Home Body"),
+                  ],
+                ),
               ),
               Container(
-                child: Text("Articles Body"),
+                child: Column(
+                  children: [
+                    Divider(color: Colors.black),
+                    Text("Home Body"),
+                  ],
+                ),
               ),
               Container(
-                child: Text("User Body"),
+                child: Column(
+                  children: [
+                    Divider(color: Colors.black),
+                    Text("Home Body"),
+                  ],
+                ),
               ),
               Container(
-                child: Text("User Body"),
+                child: Column(
+                  children: [
+                    Divider(color: Colors.black),
+                    Text("Home Body"),
+                  ],
+                ),
               ),
               Container(
-                child: Text("Home Body"),
+                child: Column(
+                  children: [
+                    Divider(color: Colors.black),
+                    Text("Home Body"),
+                  ],
+                ),
               ),
               Container(
-                child: Text("Articles Body"),
+                child: Column(
+                  children: [
+                    Divider(color: Colors.black),
+                    Text("Home Body"),
+                  ],
+                ),
               ),
               Container(
-                child: Text("User Body"),
+                child: Column(
+                  children: [
+                    Divider(color: Colors.black),
+                    Text("Home Body"),
+                  ],
+                ),
               ),
             ]),
           ),
--- lib/typography.dart
+++ lib/typography.dart
@@ -6,3 +6,5 @@ import 'colors.dart';
 final blackHeading42 = TextStyle(fontSize: 42, color: blackk, fontWeight: FontWeight.w800);
 final accentHeading32 = TextStyle(fontSize: 32, color: accentt, fontWeight: FontWeight.w800);
 final grayHeading32 = TextStyle(fontSize: 32, wordSpacing: 8, color: whitee, fontWeight: FontWeight.w900);
+
+final tabview18 = TextStyle(fontSize: 20, color: Colors.black);
